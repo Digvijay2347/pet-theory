5e160038aee32b49f45c24821aa2bd6b57380fbd
Digvijay Singh <90893525+Digvijay2347@users.noreply.github.com>
2024-09-18 19:55:23 +0530

    Update importTestData.js

--- lab01/importTestData.js
+++ lab01/importTestData.js
@@ -1,5 +1,16 @@
 const csv = require('csv-parse');
 const fs  = require('fs');
+const { Firestore } = require("@google-cloud/firestore");
+const { Logging } = require('@google-cloud/logging');
+const logName = "pet-theory-logs-importTestData";
+
+// Creates a Logging client
+const logging = new Logging();
+const log = logging.log(logName);
+
+const resource = {
+  type: "global",
+};
 
 function writeToDatabase(records) {
   records.forEach((record, i) => {
@@ -10,19 +21,20 @@ function writeToDatabase(records) {
 
 async function importCsv(csvFilename) {
   const parser = csv.parse({ columns: true, delimiter: ',' }, async function (err, records) {
-    if (e) {
-      console.error('Error parsing CSV:', e);
+    if (err) {
+      console.error('Error parsing CSV:', err);
       return;
     }
     try {
       console.log(`Call write to Firestore`);
-      await writeToDatabase(records);
+      await writeToFirestore(records);
+      // await writeToDatabase(records);
       console.log(`Wrote ${records.length} records`);
     } catch (e) {
       console.error(e);
       process.exit(1);
     }
-  }); 
+  });
 
   await fs.createReadStream(csvFilename).pipe(parser);
 }
@@ -30,6 +42,27 @@ async function importCsv(csvFilename) {
 if (process.argv.length < 3) {
   console.error('Please include a path to a csv file');
   process.exit(1);
+}
+async function writeToFirestore(records) {
+  const db = new Firestore({  
+    // projectId: projectId
+  });
+  const batch = db.batch()
+
+  records.forEach((record)=>{
+    console.log(`Write: ${record}`)
+    const docRef = db.collection("customers").doc(record.email);
+    batch.set(docRef, record, { merge: true })
+  })
+
+  batch.commit()
+    .then(() => {
+       console.log('Batch executed')
+    })
+    .catch(err => {
+       console.log(`Batch error: ${err}`)
+    })
+  return
 }
 
 importCsv(process.argv[2]).catch(e => console.error(e));
